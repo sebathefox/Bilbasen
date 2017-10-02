759370763e2b2fc66d9fbc46abc31df9d3e8acb0
Sebastian D <seba4928@edu.sde.dk>
2017-10-02 11:32:49 +0200

    v0.2

--- header.php
+++ header.php
@@ -23,7 +23,7 @@ echo '
 	<li class="dropdown">
     <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
     <div class="dropdown-content">
-      <a href="signup.php">Opret ny bruger</a>
+      <a href="#signup.php">Opret ny bruger</a>
       <a href="#">Link 2</a>
       <a href="#">Link 3</a>
     </div>
